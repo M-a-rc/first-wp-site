b66e46c08ba042e614e35658f291f746ff9c7dc2
Marc Delesalle <mrc.delesalle@gmail.com>
2022-10-29 12:40:37 +0200

    qui est valentine

--- wp-content/themes/first-site/templates/page-template-qui-est-valentine.php
+++ wp-content/themes/first-site/templates/page-template-qui-est-valentine.php
@@ -1,6 +1,6 @@
 <?php
   /*
-  Template Name: Page Services
+  Template Name: Page Qui Est Valentine ?
   */
 ?>
 
@@ -16,32 +16,44 @@
   <div class="discover-wrapper-text">
     <span class='small-golden-text'>DÉCOUVREZ</span>
     <div class="flex-column align-items-center justify-content-center">
-      <h3 class="mb-0">NOS SERVICES</h3>
+      <h3 class="mb-0">QUI EST VALENTINE ?</h3>
       <div class="mini-border-btm"></div>
     </div>
-    <p class="mt-3 text-center"><?php the_field('texte-bloc-decrouvrez-services')?></p>
+    <p class="mt-3 text-center"><?php the_field('texte-bloc-decouvrez-valentine')?></p>
   </div>
   <div class="small-collection">
     <div class="d-flex flex-column align-items-center justify-content-start">
-      <img src="http://localhost:8000/wp-content/uploads/2022/10/Controle-de-vue.png" alt="logo service 1" class="small-collection-image">
+      <img src="http://localhost:8000/wp-content/uploads/2022/10/Service-personnalise.png" alt="logo service 1" class="small-collection-image">
       <p class="text-center"><?php the_field('service-1') ?></p>
     </div>
     <div class="d-flex flex-column align-items-center justify-content-start">
-      <img src="http://localhost:8000/wp-content/uploads/2022/10/Contactologie.png" alt="logo service 2" class="small-collection-image">
+      <img src="http://localhost:8000/wp-content/uploads/2022/10/Expertise.png" alt="logo service 2" class="small-collection-image">
       <p class="text-center"><?php the_field('service-2') ?></p>
     </div>
     <div class="d-flex flex-column align-items-center justify-content-start">
-      <img src="http://localhost:8000/wp-content/uploads/2022/10/Lunettes-de-soleil.png" alt="logo service 3"  class="small-collection-image">
+      <img src="http://localhost:8000/wp-content/uploads/2022/10/Selection-exclusive.png" alt="logo service 3"  class="small-collection-image">
       <p class="text-center"><?php the_field('service-3') ?></p>
     </div>
   </div>
 </div>
 
 <div class="quarter-text-wrapper">
+  <div class="quarter-text-wrapper-text reversed">
+    <div class='quarter-text-text-area reversed blue'>
+      <h3><?php the_field('titre-bloc-bleu') ?></h3>
+      <p><?php the_field('texte-bloc-bleu') ?></p>
+      <div class="btn-principal">En savoir plus</div>
+      <div class="golden-square"></div>
+    </div>
+    <div class="quarter-text-background-image"></div>
+  </div>
+</div>
+
+<div class="quarter-text-wrapper">
   <div class="quarter-text-wrapper-text cropped">
     <div class='quarter-text-text-area cropped'>
-      <h3><?php the_field('titre-bloc-texte-gauche') ?></h3>
-      <p><?php the_field('texte-bloc-texte-gauche') ?></p>
+      <h3><?php the_field('titre-bloc-3') ?></h3>
+      <p><?php the_field('texte-bloc-3') ?></p>
       <div class="btn-principal">En savoir plus</div>
       <div class="golden-square btm"></div>
     </div>
