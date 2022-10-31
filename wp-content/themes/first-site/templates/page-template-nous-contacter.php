f4ac445a6ab2368f3c97d00a77b2057de5b80d57
Marc Delesalle <mrc.delesalle@gmail.com>
2022-10-31 18:46:02 +0100

    desktop OK

--- wp-content/themes/first-site/templates/page-template-nous-contacter.php
+++ wp-content/themes/first-site/templates/page-template-nous-contacter.php
@@ -41,6 +41,17 @@
   </div>
 </div>
 
+
+<div class="container">
+  <?php
+    if(have_posts()){
+      while(have_posts()){
+        the_post();
+        get_template_part( 'template-parts/content-page' );    }
+    }
+  ?>
+</div>
+
 </article>
 
 <?php
