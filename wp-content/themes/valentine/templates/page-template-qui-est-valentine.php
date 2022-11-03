c8ea6cb7b3a4b39618b7d7c14479d89a3d5fde02
Marc Delesalle <mrc.delesalle@gmail.com>
2022-11-03 19:48:17 +0100

    page qui est valentine avec fields

--- wp-content/themes/valentine/templates/page-template-qui-est-valentine.php
+++ wp-content/themes/valentine/templates/page-template-qui-est-valentine.php
@@ -10,7 +10,7 @@
 
 <article>
 
-<img class="small-banner" src="http://localhost:8000/wp-content/uploads/2022/10/omid-armin-Zt99Ho5Hq3s-unsplash-scaled.jpg" alt="Banner image">
+<img class="small-banner" src="<?php the_field('small-banner-image')?>" alt="Banner image">
 
 <div class="discover-wrapper">
   <div class="discover-wrapper-text">
@@ -23,15 +23,15 @@
   </div>
   <div class="small-collection">
     <div class="d-flex flex-column align-items-center justify-content-start">
-      <img src="http://localhost:8000/wp-content/uploads/2022/10/Service-personnalise.png" alt="logo service 1" class="small-collection-image">
+      <img src="<?php the_field('logo-service-1') ?>" alt="logo service 1" class="small-collection-image">
       <p class="text-center"><?php the_field('service-1') ?></p>
     </div>
     <div class="d-flex flex-column align-items-center justify-content-start">
-      <img src="http://localhost:8000/wp-content/uploads/2022/10/Expertise.png" alt="logo service 2" class="small-collection-image">
+      <img src="<?php the_field('logo-service-2') ?>" alt="logo service 2" class="small-collection-image">
       <p class="text-center"><?php the_field('service-2') ?></p>
     </div>
     <div class="d-flex flex-column align-items-center justify-content-start">
-      <img src="http://localhost:8000/wp-content/uploads/2022/10/Selection-exclusive.png" alt="logo service 3"  class="small-collection-image">
+      <img src="<?php the_field('logo-service-3') ?>" alt="logo service 3"  class="small-collection-image">
       <p class="text-center"><?php the_field('service-3') ?></p>
     </div>
   </div>
@@ -45,7 +45,7 @@
       <div class="btn-principal">En savoir plus</div>
       <div class="golden-square"></div>
     </div>
-    <div class="quarter-text-background-image"></div>
+    <div class="quarter-text-background-image" style="background-image: url(<?php the_field('image-bloc-bleu')?>)"></div>
   </div>
 </div>
 
@@ -56,7 +56,7 @@
       <p><?php the_field('texte-bloc-3') ?></p>
       <div class="btn-principal">En savoir plus</div>
     </div>
-    <div class="quarter-text-background-image cropped"></div>
+    <div class="quarter-text-background-image cropped" style="background-image: url(<?php the_field('image-bloc-3')?>)"></div>
   </div>
 </div>
 
